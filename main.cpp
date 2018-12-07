102d2c9c825c177f8594f74276a4dc51838f761f
Bartholomew <bartolson1189@gmail.com>
2018-12-06 19:00:02 -0800

    Adding Source and Include!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "include/functions.hpp"
 
 int main(){
+	int a = 8;
+	int b = 9;
+	int c = add(a, b);
+	std::cout << "\nresult is: " << c << "\n";
 	std::cout << "\nHello Meriam and Bartholomew!\n";
 	return 0;
 }
---
+++ source/functions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/functions.hpp"
+
+int add(int a, int b){
+	int result = a + b;
+	return result;
+}
